ba185af55bcb75c98148adf9b55b0a7e424d3e1a
bernkyal <bernardkya@gmail.com>
2025-08-15 03:09:23 +0100

    final tweek

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -24,7 +24,7 @@ export function Heading() {
         
         <div>
         <Flex mt="8" gap="3">
-      <Link href="/BernardCV.pdf" download>
+      <Link href="https://bernkyal.github.io/myportfolio/BernardCV.pdf" download>
       <Button variant="outline">Download CV</Button>
       </Link>
 
--- src/components/ui/linktree.tsx
+++ src/components/ui/linktree.tsx
@@ -19,7 +19,7 @@ export function LinkTree() {
         <Link href="mailto:bernardkya@gmail.com">
         <MdContactMail size="40px"/> 
         </Link>
-        <Link href="/BernardCV.pdf" download>
+        <Link href="https://bernkyal.github.io/myportfolio/BernardCV.pdf" download>
         <FaFileDownload  size="40px"/> 
         </Link>
         </VStack>    

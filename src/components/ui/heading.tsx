e2a72e67c8f6b539ca14351595dec99da4383ad0
bernkyal <bernardkya@gmail.com>
2025-08-16 12:23:23 +0100

    Fixed typo

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -22,7 +22,7 @@ export function Heading() {
         <Separator borderColor="white" w="50%" />
         <Grid w="70%">
           
-        <div><b>A Data Science enthusiast, Computer Science student who has a strong passion for machine learning and data analytics. Driven by curiousity, I'm uncovering our data's meaning to better help understand our world. As a proud Workstream Leader within my University, I'm utilizing both my technical expertise and my leadership skills to continuously apply data driven solutions while supporting and collaborating with others.</b> <br/><br/></div>
+        <div><b>A Data Science enthusiast, Computer Science student who has a strong passion for machine learning and data analytics. Driven by curiosity, I'm uncovering our data's meaning to better help understand our world. As a proud Workstream Leader within my University, I'm utilizing both my technical expertise and my leadership skills to continuously apply data driven solutions while supporting and collaborating with others.</b> <br/><br/></div>
         
         <div>
         <Flex mt="8" gap="3">
